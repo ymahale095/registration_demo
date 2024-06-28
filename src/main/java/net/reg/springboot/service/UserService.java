67b6150356673d68252ee258c8114553991a47cf
ymahale095 <113272836+ymahale095@users.noreply.github.com>
2024-06-28 18:00:18 +0530

    initial commit

---
+++ src/main/java/net/reg/springboot/service/UserService.java
@@ -0,0 +1,14 @@
+package net.reg.springboot.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import net.reg.springboot.model.User;
+import net.reg.springboot.web.dto.UserRegistrationDto;
+
+
+public interface UserService extends UserDetailsService
+
+{
+	User save(UserRegistrationDto registrationDto);
+}
+
---
+++ src/test/java/net/reg/springboot/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,15 @@
+package net.reg.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+
+
+	void contextLoads() {
+	}
+
+}
